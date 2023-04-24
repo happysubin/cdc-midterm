2bcde70b42eb3f957ca9f9f20e8e4636f460b59c
happysubin <asb0711@gmail.com>
2023-04-25 03:06:34 +0900

    feat: 프로듀서 앱 세팅

---
+++ producer-app-cdc/src/main/java/com/cdc/producerappcdc/ProducerAppCdcApplication.java
@@ -0,0 +1,13 @@
+package com.cdc.producerappcdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerAppCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerAppCdcApplication.class, args);
+	}
+
+}
---
+++ producer-app-cdc/src/test/java/com/cdc/producerappcdc/ProducerAppCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdc.producerappcdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerAppCdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
